401d99effeb9c28b49a5b292bb701e60977b5611
mehdi <mehdone@gmail.com>
2018-04-07 13:10:39 +0430

    first commit

---
+++ web/js/app.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+
+});
+
+var updateSubscribersStat = function () {
+
+};
+
+var updateMessagesStat = function () {
+
+};
+
+var inquiryMessageStat = function () {
+
+};
