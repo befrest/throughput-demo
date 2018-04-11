98e0f1242d204445f5a3d9eef6e94ca404ecaecb
Mohammad Anisi <ecut@ecut.oddrun>
2018-04-11 12:13:06 +0430

    changed boon to gson

--- app/src/main/java/rest/bef/demo/data/JsonTransformer.java
+++ app/src/main/java/rest/bef/demo/data/JsonTransformer.java
@@ -1,5 +1,6 @@
 package rest.bef.demo.data;
 
+import com.google.gson.Gson;
 import org.boon.json.JsonFactory;
 import org.boon.json.ObjectMapper;
 import spark.ResponseTransformer;
@@ -7,10 +8,12 @@ import spark.ResponseTransformer;
 public class JsonTransformer implements ResponseTransformer {
 
     private static final ObjectMapper PARSER = JsonFactory.create();
+    private static final Gson mapper = new Gson();
 
     @Override
     public String render(Object model) throws Exception {
-        return PARSER.toJson(model);
+        return mapper.toJson(model);
+//        return PARSER.toJson(model);
 /*        String result;
 
         JsonSerializer ackParser = new JsonSerializerFactory()
